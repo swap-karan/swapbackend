e01774f5d2095796709a30b1e9922d6fe44c70d0
Rajeshkmr007 <59645783+Rajeshkmr007@users.noreply.github.com>
2020-02-27 10:15:13 +0530

    allfiles

--- app/Http/Controllers/API/PagesController.php
+++ app/Http/Controllers/API/PagesController.php
@@ -197,6 +197,174 @@ class PagesController extends Controller
     return response()->json($data);
   }
 
+
+  /*     * *
+     * Services Page
+     * */
+
+  public function services()
+  {
+    $data = array(
+      'page' => 'services',
+      'page_heading' => 'Services',
+      'page_SubHeading' => 'eCommerce shop, WooCommerce, Magento 2.0, Shopify Sote, Laravel',
+
+      'about_Sec' => array(
+        'image' => 'services_lp.jpg',
+        'text' => 'Now these days most peoples having vast knowledge about ecommerce as 30-40% peoples wold like to do shopping online. And it has now become a very huge  platform seling and buying goods online. So if you have any kind of business you can take the advantage of this platform and increase your reach of your products toward existing and new customers.',
+        'text2' => 'Below we have bit explained about the layout, features and its pricing to make it clear how it look like and how its going to work etc. If you are ready to take advantage you can contact us of hire experts from the list / team.',
+        'btn_Text' => 'Let’s  Create Your Store',
+        'btn_Url' => 'https://google.com/',
+      ),
+      'pageServices' => array(
+        array(
+         'serviceSliderNum' => '01',
+         'serviceSliderTitle' => 'Home Page',
+         'serviceSliderSubtitle' => 'Why Use',
+         'serviceSliderText' => 'Home page is like a basically a main page to tell user what you have, they can buy.',
+         'serviceSliderText2' => 'You can also include featured iteams and newly added items and other sales and offers etc.',
+         'serviceSliderBtnUrl' => 'https://google.com/',
+         'serviceSliderBtn' => 'See Live website links',
+         'serviceSliderImg' => 'lp_services.jpg',
+         'serviceSliderImg2' => 'lp_services2.jpg',
+         'serviceSliderImg3' => 'lp_services3.jpg',
+         'serviceSliderImg4' => 'lp_services4.jpg',
+         'serviceSliderImg_' => 'lp_elementor1.png',
+         'serviceSliderImg2_' => 'lp_elementor2.jpg',
+         'serviceSliderImg3_' => 'lp_elementor3.jpg',
+        ),
+        array(
+         'serviceSliderNum' => '02',
+         'serviceSliderTitle' => 'Home Page',
+         'serviceSliderSubtitle' => 'Why Use',
+         'serviceSliderText' => 'Home page is like a basically a main page to tell user what you have, they can buy.',
+         'serviceSliderText2' => 'You can also include featured iteams and newly added items and other sales and offers etc.',
+         'serviceSliderBtnUrl' => 'https://google.com/',
+         'serviceSliderBtn' => 'See Live website links',
+         'serviceSliderImg' => 'lp_services.jpg',
+         'serviceSliderImg2' => 'lp_services2.jpg',
+         'serviceSliderImg3' => 'lp_services3.jpg',
+         'serviceSliderImg4' => 'lp_services4.jpg',
+         'serviceSliderImg_' => 'lp_elementor1.png',
+         'serviceSliderImg2_' => 'lp_elementor2.jpg',
+         'serviceSliderImg3_' => 'lp_elementor3.jpg',
+        ),
+      ),
+      'features' => array(
+          'featureHeading' => 'Features',
+          'featureList' => array(
+            'Flexible Payment Gateway', 'Add Unlimited Products', 'Easy Reporting / Analyticls', 'Complete User Data', 'Complete Sale / Purchase History', 'Access from anywhere', 'Responsive Layout', 'Easy to maintain and hanle',
+          ),
+          'featureImages' => array(
+            'image1' => 'feature_img.jpg',
+            'image2' => 'feature_img2.jpg',
+          ),
+          'featureTechImg' => array( 
+            array(
+              'image' => 'technology7.png',
+            ),
+            array(
+              'image' => 'technology5.png',
+            ),
+            array(
+              'image' => 'technology6.png',
+            ),
+            array(
+              'image' => 'technology11.png',
+            ),
+            array(
+              'image' => 'technology9.png',
+            ),
+            array(
+              'image' => 'technology1.png',
+            ),
+            array(
+              'image' => 'technology12.png',
+            ),
+          ),
+      ),
+      'pricing' => array(
+        array(
+          'title' => 'Shopify Store',
+          'featured' => true,
+          'price' => '2.55-4k',
+          'subtitle' => 'Negotiable',
+          'btn' => 'Get Quotation',
+          'priceFeatures' => array(
+            'Custom Design', 'Shopify Store Integration', 'Payment Gateway', 'Mega Menu', 'Limited Customization', 'Mobile Compatible', 'On Going Support',
+            ),
+          'note' => 'Best for Medium Online Store',
+        ),  
+        array(
+          'title' => 'Laravel + Vue<br />Angular and react',
+          'featured' => false,
+          'price' => '2k-20k',
+          'subtitle' => 'Negotiable',
+          'btn' => 'Get Quotation',
+          'priceFeatures' => array( 
+            'Custom Design', 'Custom Admin', 'Custom Development', 'Payment Gateway', 'Mega Menu', 'Mobile Compatible', 'Flexible in Customization', 'On Going Support',
+            ),
+          'note' => 'Best for Lage and complex online store',
+        ), 
+        array(
+          'title' => 'Magento',
+          'featured' => false,
+          'price' => '5k-10k',
+          'subtitle' => 'Negotiable',
+          'btn' => 'Get Quotation',
+          'priceFeatures' => array( 
+            'Custom Design', 'Prebuilt Admin', 'Custom Development', 'Payment Gateway', 'Mega Menu', 'Mobile Compatible', 'Complex in Customization', 'On Going Support',
+            ),
+          'note' => 'Best for Lage and complex online store',
+        ), 
+        array(
+          'title' => 'Elementor + Woo<br />Commerce',
+          'featured' => false,
+          'price' => '500-2000',
+          'subtitle' => 'Negotiable',
+          'btn' => 'Get Quotation',
+          'priceFeatures' => array(
+              'Custom Design', 'Custom Development', 'Payment Gateway', 'Mega Menu', 'Mobile Compatible', 'Flexible in Customization', 'On Going Support',
+            ),
+          'note' => 'Best for Small Online Store',
+        ), 
+      ),
+      'team' => array(
+        'heading' => 'Looking Experts for Support only?<br />Already have design?',
+        'description' => 'If you already have design and need HTML / and Integration work we can assist you with that as well.',
+        'heading2' => 'We charge for the support / assistense work @ $10-15 p/h.',
+        'hireBtn' => 'Hire Experts For Assistanse',
+        'hireBtnUrl' => 'https://google.com/',
+        'teamBtn' => 'All team members',
+        'teamBtnUrl' => 'https://google.com/',
+        'moreBtn' => 'Show More',
+      ),
+      'technologies' => array(
+
+        'tech_content' => array(
+          'heading' => 'Technologis we are working with',
+          'text' => 'We are passnate about new learning so we are not just single technologes based. We believe to keep our skils up to date and we welome our clients choice of technologies.',
+          'subheading' => 'Looking for something else?',
+          'btnText' => 'Let Us Know!',
+          'btnUrl' => 'https://google.com/',
+        ),
+        'tech_alternate' => array(
+          'heading' => 'Have an idea? We are here to discuss, we are excited to get started together.',
+          'btnText' => 'Share your requirement',
+          'btnUrl' => 'https://google.com/',
+          'getbtnText' => 'Get in Touch!',
+          'getbtnUrl' => 'https://google.com/',
+        )
+      ),
+    );
+    $data['testimonial'] = TestimonialResource::collection(Testimonial::all());
+    $data['technologies']['tech_images'] = TechnologyResource::collection(Technology::all());
+    $data['team']['team_member'] = TeamResource::collection(Team::all());
+    $data['history']['history_slider'] = TimelineResource::collection(Timeline::orderBy('year', 'ASC')->get());
+    $data = array_merge($data, $this->globalDetails());
+    return response()->json($data);
+  }
+
   /*     * *
      * Portfolio Page
      * */
@@ -266,97 +434,6 @@ class PagesController extends Controller
     $data = array_merge($data, $this->globalDetails());
     return response()->json($data);
   }
-
-  /*     * *
-     * Services Page
-     * */
-
-  public function services()
-  {
-    $data = array(
-      'page' => 'services',
-      'homeBaner' => array(
-        array(
-          'banner_img' => 'banner-slide1.jpg',
-          'banner_subheading' => 'Let’s get ',
-          'banner_heading' => 'started together',
-          'banner_description' => 'Ecommerce shop, Wordpress, Landing pages Email \n templates, Hubspot, Shopify, Laravel...',
-          'create_button' => 'Let’s  Create Your Website',
-          'take_a_look' => 'Take a look over our work'
-        ),
-        array(
-          'banner_img' => 'banner-slide1.jpg',
-          'banner_subheading' => 'Let’s get dobara ',
-          'banner_heading' => 'started sdf',
-          'banner_description' => 'Ecommerce shop, Wordpress, Landing pages Email templates,',
-          'create_button' => 'Let’s  Your Website',
-          'take_a_look' => 'Take a look over ou',
-        )
-      ),
-      "services" => array(
-        array(
-          'services_number' => '01',
-          'services_subheading' => 'Website Design & Rebranding',
-          'service_img' => 'service1.jpg',
-          'service_head' => 'Online Ecommerce Solutions',
-          'service_desc' => 'Create awesome looking and fast loading webshops and Web Application with us',
-          'service_url_text' => 'Read More',
-          'service_url' => 'https://google.com/',
-        ),
-        array(
-          'services_number' => '01',
-          'services_subheading' => 'Website Design & Rebranding',
-          'service_img' => 'service1.jpg',
-          'service_head' => 'Online Ecommerce Solutions',
-          'service_desc' => 'Create awesome looking and fast loading webshops and Web Application with us',
-          'service_url_text' => 'Read More',
-          'service_url' => 'https://google.com/',
-        ),
-        array(
-          'services_number' => '01',
-          'services_subheading' => 'Website Design & Rebranding',
-          'service_img' => 'service1.jpg',
-          'service_head' => 'Online Ecommerce Solutions',
-          'service_desc' => 'Create awesome looking and fast loading webshops and Web Application with us',
-          'service_url_text' => 'Read More',
-          'service_url' => 'https://google.com/',
-        ),
-        array(
-          'services_number' => '01',
-          'services_subheading' => 'Website Design & Rebranding',
-          'service_img' => 'service1.jpg',
-          'service_head' => 'Online Ecommerce Solutions',
-          'service_desc' => 'Create awesome looking and fast loading webshops and Web Application with us',
-          'service_url_text' => 'Read More',
-          'service_url' => 'https://google.com/',
-        ),
-      ),
-      // 'testimonial' => array(
-      //     array(
-      //         'testimonial_text' => 'Swap Development has become an asset to our team. They are website experts! They can do anything you need. From custom coding to learning new platforms, they are experts! We are so happy to find a company like Swap…. We are not ending contract, just moving some things around. Thanks again for everything your teams does!',
-      //         'testimonial_img' => 'user.jpg',
-      //         'testimonial_user' => 'Jack Ikard',
-      //         'testimonial_designation' => 'Founder at AquaSprouts LLC'
-      //     ),
-      //     array(
-      //         'testimonial_text' => 'Swap Development has become an asset to our team. They are website experts! They can do anything you need. From custom coding to learning new platforms, they are experts! We are so happy to find a company like Swap…. We are not ending contract, just moving some things around. Thanks again for everything your teams does!',
-      //         'testimonial_img' => 'user.jpg',
-      //         'testimonial_user' => 'Jack Ikard',
-      //         'testimonial_designation' => 'Founder at AquaSprouts LLC'
-      //     ),
-      //     array(
-      //         'testimonial_text' => 'Swap Development has become an asset to our team. They are website experts! They can do anything you need. From custom coding to learning new platforms, they are experts! We are so happy to find a company like Swap…. We are not ending contract, just moving some things around. Thanks again for everything your teams does!',
-      //         'testimonial_img' => 'user.jpg',
-      //         'testimonial_user' => 'Jack Ikard',
-      //         'testimonial_designation' => 'Founder at AquaSprouts LLC'
-      //     )
-      // )
-    );
-    $data['testimonial'] = TestimonialResource::collection(Testimonial::all());
-    $data = array_merge($data, $this->globalDetails());
-    return response()->json($data);
-  }
-
   /**
    * Remove the specified resource from storage.
    *
